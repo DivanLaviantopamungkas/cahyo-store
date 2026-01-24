febb0afbb96b5e76432f3d8376e04c88e42464c9
Yogaaprtma <111202113472@mhs.dinus.ac.id>
2026-01-24 13:47:36 +0700

    Modifikasi notif sukses di admin pengaturan agar tidak double

--- resources/views/admin/settings/general.blade.php
+++ resources/views/admin/settings/general.blade.php
@@ -96,8 +96,4 @@
         </form>
     </x-admin.card>
 </div>
-
-@if(session('toast'))
-    @include('components.admin.toast')
-@endif
 @endsection
--- resources/views/admin/settings/payment.blade.php
+++ resources/views/admin/settings/payment.blade.php
@@ -139,8 +139,4 @@
         </form>
     </x-admin.card>
 </div>
-
-@if(session('toast'))
-    @include('components.admin.toast')
-@endif
 @endsection
